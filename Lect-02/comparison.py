74610c7e0bb2aed10c275ecd52456179a06ea18f
Invincible-21 <shantobd1969@gmail.com>
2023-04-07 08:17:48 +0600

    updated

---
+++ Lect-02/comparison.py
@@ -0,0 +1,7 @@
+pset_time = 15
+sleep_time = 8
+print(sleep_time < pset_time)
+derive = True
+drink = False
+booth = drink and derive
+print(booth)
---
+++ Lect-02/input.py
@@ -0,0 +1,8 @@
+text = input("Type anything...")
+print(5*text)
+
+# as input gives you a string so must cast if working 
+# with numbers
+
+num = int(input("Type anything..."))
+print(5*num)
