282a912ef8428a639140a8d6deb929aa2f3ceb07
Invincible-21 <shantobd1969@gmail.com>
2023-04-07 07:06:17 +0600

    updated

---
+++ Lect-02/output.py
@@ -0,0 +1,8 @@
+x = 1;
+print(x)
+
+x_str = str(x)
+print ("My fav num is",x,".","x =",x) #py will automatically add a space for comma
+
+print("My fav num is "+x_str+". "+"x = "+x_str)
+
---
+++ Lect-02/strings.py
@@ -0,0 +1,11 @@
+hi = "hello there"
+name = "Shanto"
+greet = hi + name
+print(greet)
+
+greeting = hi + " " + name
+print(greeting)
+
+silly = hi + (" "+name)*3
+print(silly)
+
