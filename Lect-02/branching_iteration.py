7285010d555aa743de6c491959df1abbc10b1a51
Invincible-21 <shantobd1969@gmail.com>
2023-04-12 02:36:50 +0600

    updated

--- Lect-02/branching_iteration.py
+++ Lect-02/branching_iteration.py
@@ -63,10 +63,10 @@
 ## twice and flip the table any more times than that. 
 ## Hint: use a counter
 ####################
-#n = input("You are in the Lost Forest\n****************\n****************\n :)\n****************\n****************\nGo left or right? ")
-#while n == "right" or n == "Right":
+# n = input("You are in the Lost Forest\n****************\n****************\n :)\n****************\n****************\nGo left or right? ")
+# while n == "right" or n == "Right":
 #    n = input("You are in the Lost Forest\n****************\n******       ***\n  (â•¯Â°â–¡Â°ï¼‰â•¯ï¸µ â”»â”â”»\n****************\n****************\nGo left or right? ")
-#print("\nYou got out of the Lost Forest!\n\o/")
+# print("\nYou got out of the Lost Forest!\n\o/")
 
 
 
--- Lect-02/break.py
+++ Lect-02/break.py
@@ -1,5 +1,5 @@
 mysum = 0
-for i in range(5,11,2):
+for i in range(5, 11, 2):
     mysum += i
     if mysum == 5:
         break
